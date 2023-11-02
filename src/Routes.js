e541ba6f1a1ace9512b34b54d56d425d57fe32d9
mr-louback <loubacked4work@outlook.com>
2023-11-02 20:39:33 -0300

    Create Route Error

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,17 @@ import Home from "./pages/Home";
 import Api from "./pages/Api";
 import Contact from "./pages/Contact";
 import Header from "./components/Header";
+import Error from "./pages/Error";
 
 function RoutesProj() {
   return (
     <BrowserRouter>
-    <Header />
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contato" element={<Contact />} />
         <Route path="/api" element={<Api />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
 
