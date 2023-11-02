3c610f6ab072cf03c19963f37b93066dad1476a4
mr-louback <loubacked4work@outlook.com>
2023-11-02 18:30:48 -0300

    modified h1 SujeitoDev

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import './style.css';
 function Header() {
   return <header>
-    <h1>Header</h1>
+    <h1>SujeitoDev</h1>
     <div>
       <Link to="/contato">Contato</Link>
       <Link to="/api">Api</Link> 
